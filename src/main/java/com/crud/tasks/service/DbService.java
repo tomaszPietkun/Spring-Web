7e36a53e4562e56d2fd357388b5426e3ae2d8ff1
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2020-03-02 23:45:24 +0100

    SpringWeb6

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -17,7 +17,15 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Optional<Task> getTask(final Long id) {
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Optional<Task> getTask (final Long id) {
         return repository.findById(id);
+    }
+
+    public void deleteTask(final Long id) {
+        repository.deleteById(id);
     }
 }
