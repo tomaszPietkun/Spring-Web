4e44df0c7abd98a57a3759b2260ee0ca2b19dced
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2020-03-17 19:13:18 +0100

    shell script

---
+++ showtasks.bat
@@ -0,0 +1,18 @@
+:openurl
+start chrome "http://localhost:8080/crud/v1/task/getTasks"
+if "%ERRORLEVEL%" == "0" goto runcrud
+echo.
+echo Could not open url.
+goto fail
+
+:runcrud
+./runcrud.bat
+if "%ERRORLEVEL%" == "0" goto end
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo Work is finished. Tasks are showed in the browser.
